601665e564435314f4e0bc29a904413ec497a4d8
Alejandro Pelozatto <alejandrojpelozatto@gmail.com>
2025-02-28 21:38:19 -0300

    ft: remove console log from product update handler in Products component

--- frontend-admin/src/pages/products/Products.jsx
+++ frontend-admin/src/pages/products/Products.jsx
@@ -23,7 +23,6 @@ const Products = () => {
     const handleSaveProduct = async (product) => {
         try {
             if (selectedProduct) {
-                console.log("Updating product", product);
                 await updateProduct(selectedProduct._id, product);
             } else {
                 await saveProduct(product);
